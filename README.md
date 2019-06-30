fe84647be0c0f28ee7c30f31405f9f3203d5b3dd
mariaeugeniavisier@gmail.com <mariaeugeniavisier@gmail.com>
2019-06-30 14:59:52 +0200

    Primera subida

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TestManager
 This is an app to create tests or exams from a data bank.
+## Subtitulo
+Texto
+### SubSubtitulo
+texto
+
---
+++ Server/entities/response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.entities
+{
+    public class response
+    {
+        public int Id;
+        public string Text;
+        public int IdQuestion;
+        public bool IsCorrect;
+    }
+}
